ada67d79c13a8d7b876b611bb6625cd1ed2a5213
Sabahet <61169304+Sabahet@users.noreply.github.com>
2020-09-11 22:52:37 -0400

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Sabahet Alovic on 9/11/20.
+//  Copyright © 2020 Sabahet Alovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
